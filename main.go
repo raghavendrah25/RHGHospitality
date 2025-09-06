cf3646cb5bbb89e3595d73ef87bead244325171f
raghavendrah25 <152840271+raghavendrah25@users.noreply.github.com>
2025-09-06 23:25:24 +0530

    Initialized MongoDB connection and testing

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/raghavendrah25/RHGHospitality/handlers"
+	"github.com/raghavendrah25/RHGHospitality/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://administrator:mongopass@localhost:27017" // Replace with your MongoDB URI
+const dbname = "rhghospitality-dev"                               // Replace with your database name
+const userCollection = "users"                                    // Replace with your collection name
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		fmt.Println("Error connecting to MongoDB:", err)
+		return
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userCollection)
+
+	user := types.User{
+		ID:        primitive.NewObjectID().Hex(),
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+	result, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		fmt.Println("Error inserting document:", err)
+		return
+	}
+	fmt.Println("Inserted document with ID:", result.InsertedID)
+
+	var fetchedUser types.User
+	res := coll.FindOne(ctx, map[string]interface{}{"_id": user.ID}).Decode(&fetchedUser)
+	if res != nil {
+		fmt.Println("Error fetching document:", res)
+		return
+	}
+	fmt.Printf("Fetched User: %+v\n", fetchedUser)
+
 	listenAddress := flag.String("port", ":8080", "Port to run the server on")
 	flag.Parse()
 
@@ -20,7 +57,7 @@ func main() {
 	apiV1.GET("/user", handlers.HandleGetUser)
 	apiV1.GET("/user/:id", handlers.HandleGetUserByID)
 
-	err := router.Run(*listenAddress)
+	err = router.Run(*listenAddress)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
